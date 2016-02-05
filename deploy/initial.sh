039b9e47d5dc76b34321e803353dc37fb4e4899f
mi6gan <mi666gan@gmail.com>
2016-02-05 09:29:59 +0700

    fixed wrong path to static files in deploy script

--- deploy/initial.sh
+++ deploy/initial.sh
@@ -5,8 +5,7 @@ python manage.py loadfixtures
 python manage.py loadcategories
 python manage.py fakeproducts --count 1000
 python manage.py oscar_populate_countries
-cp -R oparts/fixtures/media/oparts ./media/
-cd oparts/static
+cd petshop/static
 npm install
 node_modules/grunt-cli/bin/grunt
 cd ../../
