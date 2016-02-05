16eaf476d47f45f34aad187d2a81508bf26e7aa1
mi6gan <mi666gan@gmail.com>
2016-02-05 09:55:15 +0700

    added allowed hosts to common settings

--- petshop/settings.py
+++ petshop/settings.py
@@ -20,6 +20,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+ALLOWED_HOSTS = ['pets.mi6gan.space', 'pet-zakupki.ru']
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
