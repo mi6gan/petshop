8e7538e4063fc5d5fb87f004afc2967214755f31
mi6gan <mi666gan@gmail.com>
2016-03-19 23:00:01 +0700

    fixed deploy script

--- deploy/initial.sh
+++ deploy/initial.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 python manage.py migrate --noinput
 python manage.py flush --noinput
-python manage.py loadfixtures -v 3
 python manage.py oscar_populate_countries
+python manage.py loadfixtures -v 3
 python manage.py loadproducts petshop/fixtures/products.csv --clear -v 3
 cp -R petshop/fixtures/media/petshop ./media/
 cd petshop/static
