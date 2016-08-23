b6b4dc0e4fa52cac046b48e83a7333b198c32e9e
mi6gan <mi666gan@gmail.com>
2016-08-23 13:12:07 +0300

    fixed catalogue ul css, sitemaps node middleware

--- petshop/sitemaps/middleware.py
+++ petshop/sitemaps/middleware.py
@@ -6,12 +6,11 @@ class SitemapNodeMiddleware(object):
         ctx = getattr(response, 'context_data', None)
         if ctx:
             node = None
-            for varname in 'category', 'current_page':
-                value = ctx.get(varname)
-                if isinstance(value, object):
+            for value in (ctx.get('category'),
+                    getattr(request, 'current_page', None)):
+                if value:
                     node = getattr(value, 'sitemap_node', None)
                 if node:
                     break
             ctx['sitemap_node'] = node
-            print node
         return response
